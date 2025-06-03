8edf3c2d520da26e48c5fb049c827beb8e59d57e
Ishanth288 <ishanth28.28@gmail.com>
2025-06-03 16:30:55 +0530

    Implement Lovable's latest mobile app changes and optimizations

---
+++ android/app/src/main/java/app/lovable/a3c8bd8d7a934cc2959c4ea6cb151f36/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.a3c8bd8d7a934cc2959c4ea6cb151f36;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,10 +5,10 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.a3c8bd8d7a934cc2959c4ea6cb151f36',
   appName: 'Victure Pharmacy',
   webDir: 'dist',
-  server: {
-    url: 'https://a3c8bd8d-7a93-4cc2-959c-4ea6cb151f36.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  //server: {
+  //  url: 'https://a3c8bd8d-7a93-4cc2-959c-4ea6cb151f36.lovableproject.com?forceHideBadge=true',
+  //  cleartext: true
+  //},
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
